27a361c6b430686b1b1cae16e4a4ea4fb1427360
Maits27 <maitiurlo@gmail.com>
2023-03-17 18:37:48 +0100

    1

--- pdfDownloader.py
+++ pdfDownloader.py
@@ -1,17 +1,17 @@
 import getpass
-import signal
 import sys
 import urllib
+from time import sleep
 
 import requests
 from bs4 import BeautifulSoup
-from pip import main
 
 erabiltzailea = ""
 izena = ""
 pasahitza = ""
 cookie = ""
 token = ""
+uri= ""
 
 def datuakEskatu():
     global erabiltzailea
@@ -29,32 +29,25 @@ def datuakEskatu():
 def eskaera1():
     global cookie
     global token
+    global uri
 
     metodoa = 'GET'
-    uria = 'https://egela.ehu.eus/login/index.php'
+    uri = 'https://egela.ehu.eus/login/index.php'
     # Python "hiztegi" baten moduan adierazten dira goiburuak
     goiburua = {'Host': 'egela.ehu.eus'}
     edukia = ''
 
     # "requests" liburutegia erabiliko dugu HTTP mezuak kudeatzeko
-    response = requests.request(metodoa, uria, headers=goiburua, data=edukia,
+    response = requests.request(metodoa, uri, headers=goiburua, data=edukia,
                                  allow_redirects=False)
 
     html_fitxategia = response.content
     orria = BeautifulSoup(html_fitxategia, 'html.parser')
-    form_zerrenda= orria.find_all('form', {'class':'m-t-1 ehuloginform'})
-    formularioa=form_zerrenda[0]
-    token_z=formularioa.find_all('input', {'name':'logintoken'})
-    token=token_z[0]['value']
+    formularioa= orria.find_all('form', {'class':'m-t-1 ehuloginform'})[0]
+    token=formularioa.find_all('input', {'name':'logintoken'})[0]['value']
 
-    kode = str(response.status_code)
-    deskripzio = response.reason
-    cookie = response.headers['Set-Cookie'].split(";")[0]
-    if int(kode)//100==3: lokazio=response.headers['Location']
-    else: lokazio = ""
-    printeatuEskaera(metodoa, uria, edukia)
-    printeatuErantzuna(kode, deskripzio, cookie, lokazio)
-
+    printeatuEskaera(metodoa, uri, edukia)
+    printeatuErantzuna(response)
 
 
 def eskaera2():
@@ -62,93 +55,52 @@ def eskaera2():
     global token
     global pasahitza
     global erabiltzailea
+    global uri
 
     metodoa = 'POST'
-    uria = 'https://egela.ehu.eus/login/index.php'
     # Python "hiztegi" baten moduan adierazten dira goiburuak
-    goiburua = {'Host': 'www.ehu.eus', 'Cookie': cookie,
+    goiburua = {'Host': 'egela.ehu.eus', 'Cookie': cookie,
                 'Content-Type': 'application/x-www-form-urlencoded'}
     # "requests" liburutegia erabiliko dugu HTTP mezuak kudeatzeko
     edukia = {'logintoken': token, 'username': erabiltzailea, 'password': pasahitza}
     edukia_encoded = urllib.parse.urlencode(edukia)
     goiburua['Content-Length'] = str(len(edukia_encoded))
 
-    response = requests.request(metodoa, uria, headers=goiburua,  params= edukia,
+    response = requests.request(metodoa, uri, headers=goiburua,  data= edukia,
                                 allow_redirects=False)  # berbidalketak (host, 30x kodedun erantzunak)
 
-    html_fitxategia = response.content
-    orria2 = BeautifulSoup(html_fitxategia, 'html.parser')
-
-    html_edukia = response.content
-    orria = BeautifulSoup(html_edukia, 'html.parser')  # Cookie horiek dituen orrialdea aurkitu (log-in orrialdea)
-    formulario = orria.find('body').find('form')  # Orrialde horretan dagoen betetzeko formularioa atera
-    token = formulario.find('input', {'name': 'logintoken'})['value']
-    print(token)
-
-    kode = str(response.status_code)
-    deskripzio = response.reason
-    if int(kode) // 100 == 3:
-        lokazio = response.headers['Location']
-    else:
-        lokazio = ""
-    # printeatuEskaera(metodoa, uria, edukia)
-    # printeatuErantzuna(kode, deskripzio, cookie, lokazio)
+    printeatuEskaera(metodoa, uri, edukia)
+    printeatuErantzuna(response)
 
 
 def eskaera3():
-    global pasahitza
-    global erabiltzailea
-    global cookie
-    global token
-
-    metodoa = 'POST'
-    uria = 'https://egela.ehu.eus/login/index.php'
+    metodoa = 'GET'
     # Python "hiztegi" baten moduan adierazten dira goiburuak
-    goiburua = {'Host': 'www.ehu.eus', 'Cookie': cookie, 'Content-Type': 'application/x-www-form-urlencoded'}
+    goiburua = {'Host': 'egela.ehu.eus', 'Cookie': cookie}
+    edukia = ''
+
     # "requests" liburutegia erabiliko dugu HTTP mezuak kudeatzeko
-    edukia = {'logintoken': token, 'username': erabiltzailea, 'password': pasahitza}
-    edukia_encoded = urllib.parse.urlencode(edukia)
-    goiburua['Content-Length'] = str(len(edukia_encoded))
+    response = requests.request(metodoa, uri, headers=goiburua, data=edukia,
+                                allow_redirects=False)
 
-    response = requests.request(metodoa, uria, headers=goiburua, data=edukia_encoded,
-                                allow_redirects=False)  # berbidalketak (host, 30x kodedun erantzunak)
+    printeatuEskaera(metodoa, uri, edukia)
+    printeatuErantzuna(response)
 
-    html_fitxategia = response.content
-    orria2 = BeautifulSoup(html_fitxategia, 'html.parser')
 
-    print("ORRIA DA HAU:::::::::::::::::::::::::::::::::::::"+str(response.content))
-    kode = str(response.status_code)
-    deskripzio = response.reason
-    if int(kode) // 100 == 3:
-        lokazio = response.headers['Location']
-    else:
-        lokazio = ""
-    # printeatuEskaera(metodoa, uria, edukia)
-    # printeatuErantzuna(kode, deskripzio, cookie, lokazio)
-
-    metodoa = 'POST'
-    uria = lokazio
+def eskaera4():
+    metodoa = 'GET'
     # Python "hiztegi" baten moduan adierazten dira goiburuak
-    goiburua = {'Host': 'www.ehu.eus', 'Cookie': cookie, 'Content-Type': 'application/x-www-form-urlencoded'}
+    goiburua = {'Host': 'egela.ehu.eus', 'Cookie': cookie}
+    edukia = ''
+
     # "requests" liburutegia erabiliko dugu HTTP mezuak kudeatzeko
-    edukia = {'logintoken': token, 'username': erabiltzailea, 'password': pasahitza}
-    edukia_encoded = urllib.parse.urlencode(edukia)
-    goiburua['Content-Length'] = str(len(edukia_encoded))
+    response = requests.request(metodoa, uri, headers=goiburua, data=edukia,
+                                allow_redirects=False)
 
-    response = requests.request(metodoa, uria, headers=goiburua, data=edukia_encoded,
-                                allow_redirects=False)  # berbidalketak (host, 30x kodedun erantzunak)
+    printeatuEskaera(metodoa, uri, edukia)
+    printeatuErantzuna(response)
 
-    html_fitxategia = response.content
-    orria2 = BeautifulSoup(html_fitxategia, 'html.parser')
 
-    kode = str(response.status_code)
-    deskripzio = response.reason
-    if int(kode) // 100 == 3:
-        lokazio = response.headers['Location']
-    else:
-        lokazio = ""
-    # printeatuEskaera(metodoa, uria, edukia)
-    # printeatuErantzuna(kode, deskripzio, cookie, lokazio)
 
 
 def printeatuEskaera(metodo, uri, edukia):
@@ -157,14 +109,35 @@ def printeatuEskaera(metodo, uri, edukia):
           "\nURI: " + uri +
           "\nEDUKIA: " + str(edukia))
 
-def printeatuErantzuna(status, deskribapena, cookie, location):
-    print("\nESKAERAREN EGOERA: " + status + " " + deskribapena +
-          "\nCOOKIE: " + cookie +
-          "\nLOCATION: " + location+
+
+def printeatuErantzuna(response):
+    global cookie
+    global token
+    global uri
+    kode = str(response.status_code)
+    deskripzio = response.reason
+    if int(kode) // 100 == 3:
+        lokazio = response.headers['Location']
+        uri=lokazio
+    else:
+        lokazio = ""
+    try: c = response.headers['Set-Cookie'].split(";")[0]
+    except Exception: c=cookie
+    else: cookie=c
+
+    print("\nESKAERAREN EGOERA: " + kode + " " + deskripzio +
+          "\nCOOKIE: " + c +
+          "\nLOCATION: " + lokazio+
           "\nTOKEN: " + token)
+
+    if izena in str(response.content):
+        print("\n\n"
+              "SAIOA HASITA "+izena+"!!!!!!!!!!!!!!")
+
 
 if __name__== '__main__':
     datuakEskatu()
     eskaera1()
     eskaera2()
-    #eskaera3()
+    eskaera3()
+    eskaera4()
